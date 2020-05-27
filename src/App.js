8ef319f890d3cc3b29183d4c73b10bdd4f7ec8f5
gerborisova <avoid@AVoids-MacBook-Pro.local>
2020-05-27 17:24:29 +0300

    Created new react app

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-          
+
         >
+          
           Learn React
         </a>
       </header>
